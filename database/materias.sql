df17c2b67d2489651f89979bd7000291cde968cd
Tarcisio Gabriel Philips Rodrigues <tarcisiogabriel.philips@gmail.com>
2020-01-20 17:18:39 -0300

    Teste com algumas materias prontas

---
+++ database/materias.sql
@@ -0,0 +1,119 @@
+INSERT INTO materias VALUES('materia.teste 2',
+                            'Núcleo Planck de Artes',
+                            'Teste 2',
+                            ARRAY[2, 5],
+                            ARRAY['12:00'::TIME, '19:30'::TIME],
+                            ARRAY['13:30'::TIME, '21:00'::TIME],
+                            30,
+                            ARRAY['custo extra'],
+                            ARRAY[8, 9, 10, 11]);
+
+INSERT INTO materias VALUES('1',
+                            'Núcleo Planck de Artes',
+                            'Escultura',
+                            ARRAY[2],
+                            ARRAY['14:20'::TIME],
+                            ARRAY['16:30'::TIME],
+                            20,
+                            ARRAY['custo extra'],
+                            null); 
+
+INSERT INTO materias VALUES('2',
+                            'Núcleo Planck de Artes',
+                            'Pintura',
+                            ARRAY[3],
+                            ARRAY['14:20'::TIME],
+                            ARRAY['16:30'::TIME],
+                            20,
+                            ARRAY['custo extra'],
+                            null);
+
+INSERT INTO materias VALUES('3',
+                            'Núcleo Planck de Artes',
+                            'Desenho',
+                            ARRAY[4],
+                            ARRAY['14:20'::TIME],
+                            ARRAY['16:30'::TIME],
+                            30,
+                            ARRAY['custo extra'],
+                            null);
+
+INSERT INTO materias VALUES('4',
+                            'Núcleo Planck de Artes',
+                            'Mídias Sociais',
+                            ARRAY[5],
+                            ARRAY['14:20'::TIME],
+                            ARRAY['16:30'::TIME],
+                            20,
+                            ARRAY['custo extra'],
+                             ARRAY[5, 6, 7]);
+
+INSERT INTO materias VALUES('4',
+                            'Núcleo Planck de Artes',
+                            'Mídias Sociais',
+                            ARRAY[6],
+                            ARRAY['14:20'::TIME],
+                            ARRAY['16:30'::TIME],
+                            20,
+                            ARRAY['custo extra'],
+                             ARRAY[8, 9, 10, 11]);
+
+INSERT INTO materias VALUES('5',
+                            'Núcleo Planck de Artes',
+                            'Piano/ Teclado',
+                            ARRAY[3],
+                            ARRAY['16:40'::TIME],
+                            ARRAY['18:50'::TIME],
+                            6,
+                            ARRAY['custo extra'],
+                            null);
+
+INSERT INTO materias VALUES('6',
+                            'Núcleo Planck de Artes',
+                            'Violão 1',
+                            ARRAY[4],
+                            ARRAY['14:20'::TIME],
+                            ARRAY['16:30'::TIME],
+                            10,
+                            ARRAY['custo extra'],
+                            null);
+
+INSERT INTO materias VALUES('7',
+                            'Núcleo Planck de Artes',
+                            'Guitarra/ Baixo',
+                            ARRAY[4],
+                            ARRAY['16:40'::TIME],
+                            ARRAY['18:50'::TIME],
+                            10,
+                            ARRAY['custo extra'],
+                            null);
+
+INSERT INTO materias VALUES('8',
+                            'Núcleo Planck de Artes',
+                            'Violão 2',
+                            ARRAY[5],
+                            ARRAY['14:20'::TIME],
+                            ARRAY['16:30'::TIME],
+                            10,
+                            ARRAY['custo extra'],
+                            null);
+
+INSERT INTO materias VALUES('9',
+                            'Núcleo Planck de Artes',
+                            'Teatro',
+                            ARRAY[2],
+                            ARRAY['14:20'::TIME],
+                            ARRAY['16:30'::TIME],
+                            30,
+                            ARRAY['custo extra'],
+                            ARRAY[8, 9, 10, 11]);
+
+INSERT INTO materias VALUES('10',
+                            'Núcleo Planck de Artes',
+                            'Teatro',
+                            ARRAY[6],
+                            ARRAY['14:20'::TIME],
+                            ARRAY['16:30'::TIME],
+                            30,
+                            ARRAY['custo extra'],
+                            ARRAY[5, 6, 7]);
